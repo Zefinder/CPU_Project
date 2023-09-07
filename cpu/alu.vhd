375993c7ff092015710a8d0997f092510308945f
Zefinder <jakubiak@insa-toulouse.fr>
2023-09-07 19:07:57 +0200

    Added calculation utils and began modifying ALU

--- cpu/alu.vhd
+++ cpu/alu.vhd
@@ -3,6 +3,7 @@ library utils;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 use utils.cpu_utils.all;
+use utils.cpu_calculation_utils.all;
 
 entity alu is
     port(
@@ -15,31 +16,22 @@ entity alu is
 end entity alu;
 
 architecture RTL of alu is
-    -- Used as padding for multiplication
-    constant zero_signal : std_logic_vector(output'length - 1 downto 0) := (others => '0');
-
     -- Result of operations of the ALU. Bigger than output since multiplication 
     -- can go to 2 times the size of the input
-    signal temp_output : std_logic_vector(2 * output'length - 1 downto 0);
-
-    -- Adds padding to the input std_logic_vector
-    function add_padding(std_vector : std_logic_vector(DATA_SIZE - 1 downto 0)) return std_logic_vector is
-    begin
-        return zero_signal & std_vector;
-    end function add_padding;
+    signal temp_output : result_vector_t;
 
 begin
 
     alu_selection : with selector select temp_output <=
-        std_logic_vector(unsigned(add_padding(a)) + unsigned(add_padding(b)) + c_in) when "0000", -- Addition
-        std_logic_vector(unsigned(add_padding(a)) - unsigned(add_padding(b)) - c_in) when "0001", -- Subtraction
-        std_logic_vector(unsigned(a) * unsigned(b)) when "0010", -- Multiplication
-        add_padding(a) and add_padding(b) when "0011", -- Bitwise and
-        add_padding(a) or add_padding(b) when "0100", -- Bitwise or
-        add_padding(a) xor add_padding(b) when "0101", -- Bitwise xor
-        add_padding(not a) when "0110",  -- Bitwise not
-        add_padding(a) when "0111",      -- Comparation
-        add_padding(zero_signal) when others;
+        a + b + c_in when "0000",       -- Addition
+        a - b - c_in when "0001",       -- Subtraction
+        a * b when "0010",              -- Multiplication
+        and_op(a, b) when "0011",       -- Bitwise and
+        or_op(a, b) when "0100",        -- Bitwise or
+        xor_op(a, b) when "0101",       -- Bitwise xor
+        not_op(a) when "0110",          -- Bitwise not
+        cmp(a) when "0111",             -- Comparison
+        (others => '0') when others;
 
     flags(FLAG_C) <= '1' when selector = "1000" else
                      '0' when selector = "1100" else
--- cpu/ram_memory.vhd
+++ cpu/ram_memory.vhd
@@ -7,32 +7,39 @@ use utils.cpu_utils.all;
 entity ram_memory is
     port(
         -- Clock used to update the data in memory cells
-        clk       : in  std_logic;
+        clk        : in  std_logic;
         -- Reset used to put all memory to 0
-        rst       : in  std_logic;
+        rst        : in  std_logic;
         -- Indicate if the memory cell at address __address__ must be written
-        write     : in  std_logic;
+        write      : in  std_logic;
+        -- Indicate that the offset must be used
+        use_offset : in  std_logic;
         -- Address used to read and write in the memory
-        address   : in  std_logic_vector(DATA_SIZE - 1 downto 0);
+        address    : in  std_logic_vector(DATA_SIZE - 1 downto 0);
+        -- Offset used for address
+        offset     : in  std_logic_vector(DATA_SIZE - 1 downto 0);
         -- Data to store in the memory
-        value_in  : in  std_logic_vector(DATA_SIZE - 1 downto 0);
+        value_in   : in  std_logic_vector(DATA_SIZE - 1 downto 0);
         -- Value of the cell pointed by __address__
-        value_out : out std_logic_vector(DATA_SIZE - 1 downto 0)
+        value_out  : out std_logic_vector(DATA_SIZE - 1 downto 0)
     );
 end entity ram_memory;
 
 -- TODO Add address offset (add a 3rd register output...)
 architecture RTL of ram_memory is
     -- Memory that goes from 0 to the max number an address can go
-    signal memory : t_memory_array(0 to 2 ** address'length - 1);
-
+    signal memory         : t_memory_array(0 to 2 ** address'length - 1);
+    signal lookup_address : std_logic_vector(DATA_SIZE - 1 downto 0);
 begin
+--    lookup_address <= address + offset when use_offset = '1' else
+--                      address;
+
     write_memory : process(clk, rst) is
     begin
         if rst = '1' then
             memory <= (others => (others => '0'));
         elsif rising_edge(clk) and write = '1' then
-            memory(to_integer(unsigned(address))) <= value_in;
+            memory(to_integer(unsigned(lookup_address))) <= value_in;
         end if;
     end process write_memory;
 
